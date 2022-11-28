4edfe9e2414dc91bb2cfc4c2aa6f1086b8549420
LAPTOP-I3P3593I\Guillermo <guillermo.jabato.p@gmail.com>
2022-11-28 21:52:42 +0100

    Main a medias

--- ProyectoFinal/src/Main.java
+++ ProyectoFinal/src/Main.java
@@ -1,4 +1,19 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+        Session sesion = getCurrentSessionFromConfig();
+
+    }
+
+    public static Session getCurrentSessionFromConfig() {
+        // SessionFactory in Hibernate 5 example
+        Configuration config = new Configuration();
+        config.configure();
+        // local SessionFactory bean created
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        return sessionFactory.getCurrentSession();
     }
 }
